8f7e35993bec9b966ff686dc4ea6aa52f7dda641
Sam Boosalis <samboosalis@gmail.com>
2016-09-04 16:38:26 +0300

    docs

--- sources/Export.hs
+++ sources/Export.hs
@@ -6,7 +6,9 @@
 -- * C foreign functions
 -- * and more
 --
--- example:
+-- *
+--
+-- Example:
 --
 -- > {-# LANGUAGE DataKinds, ConstraintKinds #-}
 -- > {-# LANGUAGE NoMonomorphismRestriction #-}
@@ -16,41 +18,62 @@
 -- import Foreign
 -- import qualified Data.ByteString.Lazy.Char8 as B
 -- @
+--
+-- **
+--
+-- First, we uncurry the function:
+--
 -- @
--- -- 1. uncurry any haskell function
--- u_or = 'newFunction' ('P'::P "or") (||)
---
--- -- :: Function I I "or" '[Bool, Bool] Bool
--- -- (inferred)
+-- hs_or = 'newFunction' ('P'::P "or") (||)
+-- -- (inferred) hs_or :: Function 'I' I "or" '[Bool, Bool] Bool
 -- @
+--
+-- which means:
+--
+-- * @hs_or@ takes two bools to a bool (@'[Bool, Bool]@ is a type-level list, which are n-ary tuples).
+-- * it's named @"or"@ (which information is available at the type level)
+-- * each input lives in the identity monad.
+-- * the output also lives in the identity monad.
+--
+-- (the @hs_@ prefix implies it's being exported, as a @c_@ implies we're importing a foreign C function).
+-- 
+-- 'newFunction' works on any Haskell function (any arity, any types).
+-- 
+-- We can call normal(/curried) functions (obviously):
+--
+-- >>> (||) False True
+-- True
+--
+-- Likewise, we can call the uncurried 'Function':
+--
+-- >>> call hs_or (I False :& I True :& Z)  -- see 'call', 'I', ':&', 'Z'
+-- True
+--
+-- Or more readably:
+--
+-- >>> hs_or \`call` (False :* True :* Z)    -- see 'call', ':*'
+-- True
+--
+-- @(False :* True :* Z)@ is an unlabelled record @{False, True}@.
+--
+-- ** Second, we make the function marshallable.
+--
+-- *** Marshall via 'String':
+--
 -- @
--- -- 2. make it marshallable
--- main = do
---
---  -- curried:
---
---  print $ (||) False True
---  -- ==> True
---
---  -- un-marshalled:
---
---  print $ u_or  \`call`  ('I' False :& I True :& Z)  -- 'call'
---  -- ==> True
---
---  -- marshalled via 'String':
---  let hs_String_or = 'stringMarshaller' \`marshalling` u_or   -- 'marshalling'
+--  let hs_String_or = 'stringMarshaller' \`marshalling` hs_or   -- 'marshalling'
 --
 --  print $ hs_String_or \`call_` ('C' \"False\" :& C \"True\" :& Z)  -- 'call_'
 --  -- ==> Just \"True"
 --
 --  -- marshalled via JSON ('B.ByteString'):
---  let hs_JSON_or = jsonMarshaller \`marshalling` u_or
+--  let hs_JSON_or = jsonMarshaller \`marshalling` hs_or
 --
 --  print $ hs_JSON_or \`call_` (B.pack "false" ':#' B.pack "true" :# Z)
 --  -- ==> Right \"true"
 --
 --  -- marshalled via "Foreign.Ptr":
---  let hs_Ptr_or = 'pointerMarshaller' \`marshalling` u_or
+--  let hs_Ptr_or = 'pointerMarshaller' \`marshalling` hs_or
 --
 --  pFalse <- 'new' False
 --  pTrue  <- new True
@@ -58,6 +81,7 @@
 --  print =<< 'peek' pOr
 --  -- ==> True
 -- @
+--
 -- @
 -- 3. \"export\" it
 -- TODO
@@ -65,9 +89,12 @@
 --
 -- related:
 --
--- * <https://github.com/nh2/call-haskell-from-anything call-haskell-from-anything>
---
 -- * <https://haskell-servant.github.io/ servant>
+-- * <https://github.com/nh2/call-haskell-from-anything call-haskell-from-anything>
+-- * <http://hackage.haskell.org/package/optparse-generic-1.1.0/docs/Options-Generic.html optparse-generic>
+-- * <http://hackage.haskell.org/package/server-generic-1.0.0/docs/Server-Generic.html server-generic>
+-- * <https://hackage.haskell.org/package/typed-spreadsheet-1.1.0/docs/Typed-Spreadsheet.html typed-spreadsheet>
+-- * < >
 --
 module Export
  ( module Export.Marshall
